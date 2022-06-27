eb9ac7ebe97566c06c44e3da7ea492ce2657e54f
kangkyongsuk <imagine798@gmail.com>
2022-06-27 17:55:21 +0900

    first commit

---
+++ src/test/java/com/mycom/member/JenkinsprojectGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsprojectGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
